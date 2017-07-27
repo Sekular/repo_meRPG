789555fe22a38d8f6bdb27ac3171995562946a5d
Sekular <benjaminherron01@gmail.com>
2017-07-27 15:46:23 +1000

    - ActorStats refactored. - Cover icons updated.

--- Assets/Scripts/ActorStats.cs
+++ Assets/Scripts/ActorStats.cs
@@ -4,32 +4,24 @@ using UnityEngine;
 
 public class ActorStats : MonoBehaviour {
 
-	public RectTransform canvasRectT;
-	public RectTransform coverIcon;
-	public Vector2 coverIconOffset;
-	public RectTransform shieldBar;
-	public Vector2 shieldBarOffset;
-	public RectTransform healthBar;
-	public Vector2 healthBarOffset;
-	public Canvas canvas;
-	public float barWidth = 75f;
-	public RectTransform m_shieldPip = null;
-	public RectTransform m_healthPip = null;
+  public Canvas m_statCanvas;   // The canvas that contains all stat UI elements.
+  public RectTransform m_coverIcon, m_shieldBar, m_healthBar, m_shieldPip, m_healthPip;   // UI elements contained in the StatCanvas.
+  public Vector2 m_vCoverIconOffset = new Vector2(18.5f, 23.5f);    // Offset for placing the Cover Icon.
+  public Vector2 m_vShieldBarOffset = new Vector2(1f, 40f);   // Offset for placing the Shield Bar.
+  public Vector2 m_vHealthBarOffset = new Vector2(1f, 40f);   // Offset for placing the Health Bar.
+	public float m_fBarWidth = 75f;   // How wide Health & Shield bars should be when they're full.
 
-	private Actor actor = null;
-	private float healthPipWidth = 0f;
-	private float shieldPipWidth = 0f;
-	private float pipHeight = 5f;
-	private Vector2 shieldOrigin = new Vector2(60, 50);
-	private Vector2 healthOrigin = new Vector2(60, 45);
-	public List<RectTransform> healthPips, shieldPips = null;
+	private Actor actor = null;   // The Actor that this ActorStats belongs to.
+	private float healthPipWidth = 5f, shieldPipWidth = 5f, pipHeight = 5f;   // Used to dynamically set the width/height of each health/shield pip to fit on their respective bar.
+  private Vector2 shieldOrigin = new Vector2(60, 50), healthOrigin = new Vector2(60, 45);   // Origin points for placing the shield/health pips along their respective bar.
+  private List<RectTransform> healthPips = new List<RectTransform>(), shieldPips = new List<RectTransform>();    // Lists of the current health/shield pips. Used to display health/damage.
 
-	public void Deactivate() { canvas.gameObject.SetActive(false); }
+  public void Deactivate() { m_statCanvas.gameObject.SetActive(false); }
 
 	void Awake() {
 		actor = this.GetComponent<Actor>();
-		healthPipWidth = barWidth / actor.maxHealth;
-		shieldPipWidth = barWidth / actor.maxShield;
+    healthPipWidth = m_fBarWidth / actor.maxHealth;
+    shieldPipWidth = m_fBarWidth / actor.maxShield;
 		GenerateStatBars();
 	}
 
@@ -37,7 +29,7 @@ public class ActorStats : MonoBehaviour {
 		for (int i = 0; i < actor.maxShield; i++) {
 			RectTransform temp = Instantiate(m_shieldPip, transform.position, Quaternion.identity);
 			shieldPips.Add(temp);
-			temp.SetParent(shieldBar.transform);
+      temp.SetParent(m_shieldBar.transform);
 			temp.sizeDelta = new Vector2(shieldPipWidth, pipHeight);
 			temp.localScale = Vector3.one;
 			temp.anchoredPosition = shieldOrigin + new Vector2((shieldPipWidth * i), 0f);
@@ -46,7 +38,7 @@ public class ActorStats : MonoBehaviour {
 		for (int i = 0; i < actor.maxHealth; i++) {
 			RectTransform temp = Instantiate(m_healthPip, transform.position, Quaternion.identity);
 			healthPips.Add(temp);
-			temp.SetParent(healthBar.transform);
+      temp.SetParent(m_healthBar.transform);
 			temp.sizeDelta = new Vector2(healthPipWidth, pipHeight);
 			temp.localScale = Vector3.one;
 			temp.anchoredPosition = healthOrigin + new Vector2((healthPipWidth * i), 0f);
@@ -74,25 +66,25 @@ public class ActorStats : MonoBehaviour {
 	}
 
 	void Update() {
-
-		//first you need the RectTransform component of your canvas
-		RectTransform CanvasRect = canvas.GetComponent<RectTransform>();
-
-		//then you calculate the position of the UI element
-		//0,0 for the canvas is at the center of the screen, whereas WorldToViewPortPoint treats the lower left corner as 0,0. Because of this, you need to subtract the height / width of the canvas * 0.5 to get the correct position.
-
-		Vector2 ViewportPosition = Camera.main.WorldToViewportPoint(transform.position);
-		Vector2 WorldObject_ScreenPosition = new Vector2(
-		((ViewportPosition.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
-		((ViewportPosition.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
-
-		Vector2 coverIconPos = WorldObject_ScreenPosition + coverIconOffset;
-		Vector2 shieldBarPos = WorldObject_ScreenPosition + shieldBarOffset;
-		Vector2 healthBarPos = WorldObject_ScreenPosition + healthBarOffset;
-
-		//now you can set the position of the ui element
-		coverIcon.anchoredPosition = coverIconPos;
-		shieldBar.anchoredPosition = shieldBarPos;
-		healthBar.anchoredPosition = healthBarPos;
+    PositionStatElements();
 	}
+
+  // Used to calculate and position the UI elements in screen space based on the Actors current location on screen.
+  void PositionStatElements()
+  {
+    RectTransform CanvasRect = m_statCanvas.GetComponent<RectTransform>();
+
+    Vector2 ViewportPosition = Camera.main.WorldToViewportPoint(transform.position);
+    Vector2 WorldObject_ScreenPosition = new Vector2(
+    ((ViewportPosition.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
+    ((ViewportPosition.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+
+    Vector2 coverIconPos = WorldObject_ScreenPosition + m_vCoverIconOffset;
+    Vector2 shieldBarPos = WorldObject_ScreenPosition + m_vShieldBarOffset;
+    Vector2 healthBarPos = WorldObject_ScreenPosition + m_vHealthBarOffset;
+
+    m_coverIcon.anchoredPosition = coverIconPos;
+    m_shieldBar.anchoredPosition = shieldBarPos;
+    m_healthBar.anchoredPosition = healthBarPos;
+  }
 }
